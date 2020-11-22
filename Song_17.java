5b0b33fe6840d08d2a9449f90b9d1d7cc9bc762d
raiagrigorova2 <72023155+raiagrigorova2@users.noreply.github.com>
2020-11-23 01:14:04 +0200

    Update Song_17

--- Song_17.java
+++ Song_17.java
@@ -3,7 +3,6 @@ package OOP;
 public class Song_17 {
 	private double length;
 	private String title;
-	private Album_17 album;
 	private SongLyrics_17 lyrics;
 	
 	Song_17(){
@@ -11,10 +10,9 @@ public class Song_17 {
 		setTitle("");
 	}
 	
-	Song_17(double length, String title, Album_17 album, SongLyrics_17 lyrics){
+	Song_17(double length, String title, SongLyrics_17 lyrics){
 		this.length = length;
 		this.title = title;
-		this.album = album;
 		this.lyrics = lyrics;
 	}
 
@@ -40,13 +38,5 @@ public class Song_17 {
 
 	public void setLyrics(SongLyrics_17 lyrics) {
 		this.lyrics = lyrics;
-	}
-
-	public Album_17 getAlbum() {
-		return album;
-	}
-
-	public void setAlbum(Album_17 album) {
-		this.album = album;
 	}
 }
