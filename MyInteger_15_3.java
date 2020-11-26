ca4dcb6d677611268a9cdd4d8f200ddf90bafaa4
raiagrigorova2 <72023155+raiagrigorova2@users.noreply.github.com>
2020-11-26 16:55:16 +0200

    15.2 Задача

--- MyInteger_15_3.java
+++ MyInteger_15_3.java
@@ -1,23 +1,6 @@
 package OOP;
 
 public class MyInteger_15_3 {
-//	Design a class named MyInteger . The class contains:
-//		An int data field named value that stores the int value represented by
-//		this object.
-//		A constructor that creates a MyInteger object for the specified int value.
-//		A getter method that returns the int value.
-//		The methods isEven() , isOdd() , and isPrime() that return true if the
-//		value in this object is even, odd, or prime, respectively.
-//		The static methods isEven(int) , isOdd(int) , and isPrime(int) that
-//		return true if the specified value is even, odd, or prime, respectively.
-//		The static methods isEven(MyInteger) , isOdd(MyInteger) , and
-//		isPrime(MyInteger) that return true if the specified value is even, odd,
-//		or prime, respectively.
-//		The methods equals(int) and equals(MyInteger) that return true if
-//		the value in this object is equal to the specified value.
-//		A static method parseInt(char[]) that converts an array of numeric
-//		characters to an int value.
-//		A static method parseInt(String) that converts a string into an int value.
 	
 	private int value;
 	
