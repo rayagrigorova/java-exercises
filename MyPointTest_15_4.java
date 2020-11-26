1ce4e8723cb728e0a9240a700a27757b358329bb
raiagrigorova2 <72023155+raiagrigorova2@users.noreply.github.com>
2020-11-26 16:57:12 +0200

    15.3 Упражнение

--- MyPointTest_15_4.java
+++ MyPointTest_15_4.java
@@ -8,7 +8,7 @@ public class MyPointTest_15_4 {
 		MyPoint_15_4 p2 = new MyPoint_15_4(-3,1);
 		MyPoint_15_4 p3 = new MyPoint_15_4(4,-2);
 		MyPoint_15_4 p4 = new MyPoint_15_4(6,-3);
-		
+		//
 		System.out.println(p1.distance(p2) + " " + p2.distance(p3));
 		System.out.println(p1.distance(0,0) + " " + p2.distance(-3,4));
 		System.out.println(MyPoint_15_4.distance(p1, p2));
